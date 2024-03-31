d11e36b1d62d21c5274560be995ae33969854b30
Satyabrata Nath <93023257+nathsatyabrata@users.noreply.github.com>
2024-03-31 16:04:10 +0530

    Create condition.tf

---
+++ condition.tf
@@ -0,0 +1,33 @@
+#Conditional-Expressions
+
+variable "environment" {
+  description = "Environment type"
+  type = string
+  default = "development"
+}
+
+variable "production_subnet_cidr" {
+    description = "CIDR block for Production subnet"
+    type = string
+    default = "10.0.1.0/24"
+  
+}
+
+variable "development_subnet_cidr" {
+    description = "CIDR block for develpoment subnet"
+    type = string
+    default = "10.0.2.0/24"
+  
+}
+
+resource "aws_security_group" "example" {
+    name = "example-sg"
+    description = "Example security group"
+  
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = var.environment == "production" ? [var.production_subnet_cidr] : [var.development_subnet_cidr]
+  }
+}
